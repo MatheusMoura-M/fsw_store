eaebfc14d2da06d1dead05077c20d732e69c3db2
Matheus <matheus150101miranda@gmail.com>
2024-06-07 16:08:14 -0300

    feat: ensure user goes to catalog page on catalog button click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -15,7 +15,13 @@ import { Avatar, AvatarImage } from "./avatar";
 import { Button } from "./button";
 import { Card } from "./card";
 import { Separator } from "./separator";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "./sheet";
 
 const Header = () => {
   const { status, data } = useSession();
@@ -96,10 +102,17 @@ const Header = () => {
               Ofertas
             </Button>
 
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <ListOrderedIcon size={16} />
-              Catálogo
-            </Button>
+            <SheetClose asChild>
+              <Link href="/catalog">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <ListOrderedIcon size={16} />
+                  Catálogo
+                </Button>
+              </Link>
+            </SheetClose>
           </div>
         </SheetContent>
       </Sheet>
