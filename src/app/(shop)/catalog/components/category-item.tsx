3d7393872b0ec84c86ee3ba385845e242a7fa68e
Matheus <matheus150101miranda@gmail.com>
2024-06-19 16:24:53 -0300

    style: update

--- src/app/(shop)/catalog/components/category-item.tsx
+++ src/app/(shop)/catalog/components/category-item.tsx
@@ -10,7 +10,7 @@ const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
     <Link href={`/category/${category.slug}`}>
       <div className="flex flex-col">
-        <div className="bg-category-item-gradient flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg">
+        <div className="flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg bg-category-item-gradient">
           {/* IMAGEM */}
           <Image
             src={category.imageUrl}
