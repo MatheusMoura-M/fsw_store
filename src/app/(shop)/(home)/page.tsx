99d2fcfaa3e15a5b6713b123335478890828f18b
Matheus <matheus150101miranda@gmail.com>
2024-06-19 16:24:35 -0300

    feat: update home page

--- src/app/(shop)/(home)/page.tsx
+++ src/app/(shop)/(home)/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
 import Categories from "./components/categories";
-import ProductList from "@/components/ui/product-list";
 import { prismaClient } from "@/lib/prisma";
-import SectionTitle from "@/components/ui/section-title";
+import ProductList from "../../../components/ui/product-list";
+import SectionTitle from "../../../components/ui/section-title";
 import PromoBanner from "./components/promo-banner";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -31,43 +32,81 @@ export default async function Home() {
   });
 
   return (
-    <div className="flex flex-col gap-8 py-8">
-      <PromoBanner
-        src="/banner-home-01.png"
-        alt="Até 55% de desconto esse mês!"
-        className="px-5 lg:hidden"
-      />
-
-      <div className="px-5">
-        <Categories />
+    <>
+      <div className="mx-auto max-w-[1920px]">
+        <Link href="/deals">
+          <PromoBanner
+            src="/deals-banner.png"
+            className="hidden h-auto w-full lg:block"
+            alt="Até 55% de desconto esse mês!"
+          />
+        </Link>
       </div>
 
-      <div className="">
-        <SectionTitle className="pl-5">Ofertas</SectionTitle>
-        <ProductList products={deals} />
+      <div className="mx-auto flex flex-col gap-8 py-8 lg:container lg:gap-10">
+        <Link href="/deals">
+          <PromoBanner
+            src="/banner-home-01.png"
+            alt="Até 55% de desconto esse mês!"
+            className="px-5 lg:hidden"
+          />
+        </Link>
+
+        <div className="px-5 lg:mt-2">
+          <Categories />
+        </div>
+
+        <div className="flex flex-col gap-3 lg:gap-5">
+          <SectionTitle className="pl-5">Ofertas</SectionTitle>
+          <ProductList products={deals} />
+        </div>
+
+        <div className="flex flex-col lg:flex-row">
+          <Link href="/category/mouses" className="flex flex-1">
+            <PromoBanner
+              src="/banner-home-02.png"
+              alt="Até 55% de desconto em mouses!"
+              className="w-0 flex-1 px-5"
+            />
+          </Link>
+
+          <Link href="/category/headphones" className="flex flex-1">
+            <PromoBanner
+              src="/banner-home-03.png"
+              alt="Até 55% de desconto em fones!"
+              className="hidden w-0 flex-1 lg:block"
+            />
+          </Link>
+        </div>
+
+        <div className="flex flex-col gap-3 lg:gap-5">
+          <SectionTitle className="pl-5">Teclados</SectionTitle>
+          <ProductList products={keyboards} />
+        </div>
+
+        <div>
+          <Link href="/category/headphones">
+            <PromoBanner
+              src="/banner-home-03.png"
+              alt="Até 55% de desconto em mouses!"
+              className="px-5 lg:hidden"
+            />
+          </Link>
+
+          <Link href="/catalog">
+            <PromoBanner
+              src="/free-shipping-banner.png"
+              alt="Até 55% de desconto em mouses!"
+              className="hidden px-5 lg:block"
+            />
+          </Link>
+        </div>
+
+        <div className="flex flex-col gap-3 lg:gap-5">
+          <SectionTitle className="pl-5">Mouses</SectionTitle>
+          <ProductList products={mouses} />
+        </div>
       </div>
-
-      <PromoBanner
-        src="/banner-home-02.png"
-        alt="Até 55% de desconto em mouses!"
-        className="w-0 flex-1 px-5"
-      />
-
-      <div className="flex flex-col gap-3 lg:gap-5">
-        <SectionTitle className="pl-5">Teclados</SectionTitle>
-        <ProductList products={keyboards} />
-      </div>
-
-      <PromoBanner
-        src="/banner-home-03.png"
-        alt="Até 55% de desconto em fones!"
-        className="hidden w-0 flex-1 lg:block"
-      />
-
-      <div className="flex flex-col gap-3 lg:gap-5">
-        <SectionTitle className="pl-5">Mouses</SectionTitle>
-        <ProductList products={mouses} />
-      </div>
-    </div>
+    </>
   );
 }
